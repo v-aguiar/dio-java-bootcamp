269b2dac53352ba28fa16e05eda172cfdab16255
v-aguiar <v.aguiar.dev@gmail.com>
2022-10-19 23:29:25 -0300

    chore: types and variables class

--- java_beginners/src/dio/v_aguiar/sintaxe/TiposVariaveis.java
+++ java_beginners/src/dio/v_aguiar/sintaxe/TiposVariaveis.java
@@ -3,5 +3,13 @@ package dio.v_aguiar.sintaxe;
 public class TiposVariaveis {
     public static void main(String[] args) {
         // tipos variáveis
+        // float precisa terminar com "f" maiúsculo ou minúsculo
+        // long precisa terminar com "l" maiúsculo ou minúsculo
+
+        long cpf = 12312312388L;
+        float pi = 3.14F;
+
+        System.out.println(cpf);
+        System.out.println(pi);
     }
 }
